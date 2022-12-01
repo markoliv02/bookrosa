5a15d94138b772015f7281783b6f6337107bff83
MarkOliv <marcosgarcia.oliveira02@gmail.com>
2022-12-01 10:29:17 -0300

    ajuste na cor da pagina

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+
+  return (
+    <div className={router.pathname !== "/" ? "bg-white" : ""}>
+      <Component {...pageProps} />;
+    </div>
+  );
 }
