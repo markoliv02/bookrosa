fd0d75b4b7cbbd881cf668bc626e1c59f26e7a73
MarkOliv <marcosgarcia.oliveira02@gmail.com>
2022-12-16 20:18:04 -0300

    fix quality of button

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,8 @@ import Image from "next/image";
 
 import botaoX from "../assets/botaox.svg";
 import botaoVoltar from "../assets/botaoVoltar.svg";
-import botaoMatchGold from "../assets/botaoMatch.svg";
-import botaoMatch from "../assets/botaoMatch2.svg";
-import botaoMatch2 from "../assets/botaoMatch2.svg";
+
+import botaoMatch from "../assets/botaoMatchP.png";
 
 import verificada from "../assets/verificadoIcon.svg";
 import logoGold from "../assets/logoRosa.png";
