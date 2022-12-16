df77aff05418afb3d37512e8dc8180ba29e25608
MarkOliv <marcosgarcia.oliveira02@gmail.com>
2022-12-16 17:56:41 -0300

    test

--- env.ts
+++
@@ -1,7 +0,0 @@
-const Environment = {
-  SUPABASE_URL: "https://viudhkddfyymxinmimyo.supabase.co",
-  SUPABASE_API:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpdWRoa2RkZnl5bXhpbm1pbXlvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njk2ODI1MjUsImV4cCI6MTk4NTI1ODUyNX0.Qw6EbrToBmwirquUFE7LGXdFIXH7Om6eWSn4wCzFxQ0",
-};
-
-export default Environment;
